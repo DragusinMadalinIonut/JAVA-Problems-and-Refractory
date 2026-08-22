3827a1b64d1bb778287864a05f2cacd10deabe84
John_Bukowsky <dragusin.madalin.ionut@gmail.com>
2026-08-22 22:46:22 +0300

    fixed the declaration

--- src/Refractoring/ATMAccessBad.java
+++ src/Refractoring/ATMAccessBad.java
@@ -42,6 +42,31 @@ public class ATMAccessBad {
 		System.out.println(user.state);
 		user.verifyPin(1234);
 	}
+	class ATMCardState {
+		private final String name;
+		private ATMCardState(String name) {
+			this.name = name;
+		}
+		public String toString() {
+			return name;
+		}
+		/*
+		public final static String CARD_ENTERED = "CARD ENTERED";
+		public final static String VALID_CARD = "VALID CARD";
+		public final static String VALID_PIN = "VALID PIN";
+		public final static String VALID_CASH_REQUEST = "VALID_CASH_REQUEST";
+		public final static String DEBNIED = "DENIED";
+		*/
+		public final ATMCardState CARD_ENTERED = new ATMCardState("CARD ENTERED");
+		public final ATMCardState  VALID_CARD =  new ATMCardState("VALID CARD");
+		public final ATMCardState  VALID_PIN = new ATMCardState("VALID_PIN");
+		public final ATMCardState  VALID_CASH_REQUEST = new ATMCardState("VALID_CASH_REQUEST");
+		public final ATMCardState  DEBNIED = new ATMCardState("DENIED");
+		
+		public final static int CARD_NUMBER = 123456789;
+		public final static int PIN_NUMBER = 1234;
+		public final static double CARD_BALNCE = 100.00;
+	}
 }
 
 
